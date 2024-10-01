1b047a10c792d0b36a202453a2423074876007fa
fuglesang <me@fuglesang.dev>
2024-10-01 18:57:20 +0200

    snow

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,67 +2,77 @@ extern crate crossterm;
 
 use crossterm::style::Stylize;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use crossterm::{cursor, cursor::MoveTo, execute, style, terminal, terminal::{Clear, ClearType}, QueueableCommand};
-use std::io::{stdout, Write};
+use crossterm::{cursor, cursor::MoveTo, event, execute, queue, style, terminal, terminal::{Clear, ClearType}, QueueableCommand};
+use std::io::{stdout, Stdout, Write};
+use std::{thread, time};
+use std::time::Instant;
+use time::Duration;
+use event::Event;
+
+fn delta_time(current_time: &mut Instant) -> f64 {
+    let new_time = Instant::now();
+    let dt = new_time.duration_since(*current_time).as_secs_f64();
+    *current_time = new_time;
+    dt
+}
+
+struct Snowflake {
+    x: f64,
+    y: f64,
+    speed: f64,
+}
 
 fn main() {
-    execute!(stdout(), terminal::EnterAlternateScreen).unwrap();
+    let width = 200;
+    let height = 30;
+    let mut stdout = stdout();
 
     enable_raw_mode().unwrap();
-    execute!(stdout(), cursor::Hide).unwrap();
 
-    let mut stdout = stdout();
-    let mut x = 0.0;
-    let mut y = 0.0;
+    queue!(
+        stdout,
+        terminal::EnterAlternateScreen,
+        cursor::Hide
+    ).unwrap();
+    stdout.flush().unwrap();
+
+    let mut snow_flakes: Vec<Snowflake> = Vec::new();
+    for _ in 0..100 {
+        snow_flakes.push(Snowflake {
+            x: (width as f64 * rand::random::<f64>()).floor(),
+            y: (height as f64 * rand::random::<f64>()).floor(),
+            speed: (rand::random::<f64>() * 0.5) + 2.8,
+        });
+    }
+
     let mut phase = 0.0;
 
-    let width = 80;
-    let height = 30;
-
-    let mut dt = 1.0 / 60.0;
-    let mut current_time = std::time::Instant::now();
+    let mut dt;
+    let mut current_time = Instant::now();
 
     loop {
-        let new_time = std::time::Instant::now();
-        dt = new_time.duration_since(current_time).as_secs_f64();
-        current_time = new_time;
+        dt = delta_time(&mut current_time);
 
-        stdout.queue(MoveTo(0, 0)).unwrap();
-        stdout.queue(Clear(ClearType::All)).unwrap();
-
-        // for i in 0..height {
-        //     for j in 0..width {
-        //         stdout.queue(MoveTo(j, i)).unwrap();
-        //         stdout.queue(style::PrintStyledContent("·".grey())).unwrap();
-        //     }
-        // }
+        queue!(
+            stdout,
+            MoveTo(0, 0),
+            Clear(ClearType::All)
+        ).unwrap();
 
         phase += 1.0 * dt;
-        x = (width as f64 / 2.0) + (-(phase * 2.0).sin() * (width as f64 / 2.1));
-        y = (height as f64 / 2.0) + ((phase * 2.0).cos() * (height as f64 / 2.1));
 
-        // stdout.queue(MoveTo(clamped_x, clamped_y)).unwrap();
-        // stdout.queue(style::PrintStyledContent(x.to_string().magenta())).unwrap();
-
-        stdout.queue(MoveTo(x as u16 % width, y as u16 % height)).unwrap();
-        stdout.queue(style::PrintStyledContent("◯".green())).unwrap();
-
-        for i in 0..width {
-            let x = i as f64;
-            let y = (height as f64 / 2.0) + (phase + x / 10.0).sin() * 10.0;
-            let clamped_y = y as u16 % height;
-            stdout.queue(MoveTo(x as u16, clamped_y)).unwrap();
-            stdout.queue(style::PrintStyledContent("█".magenta())).unwrap();
-        }
+        draw_dots(&mut stdout, width, height);
+        draw_sine_wave(&mut stdout, width, height, phase);
+        draw_snow_flakes(&mut stdout, width, height, phase, dt, &mut snow_flakes);
 
         stdout.queue(MoveTo(0, 0)).unwrap();
         stdout.flush().unwrap();
 
-        std::thread::sleep(std::time::Duration::from_millis(100));
+        thread::sleep(Duration::from_millis(100));
 
-        if crossterm::event::poll(std::time::Duration::from_millis(0)).unwrap() {
-            if let crossterm::event::Event::Key(event) = crossterm::event::read().unwrap() {
-                if event.code == crossterm::event::KeyCode::Char('q') {
+        if event::poll(Duration::from_millis(0)).unwrap() {
+            if let Event::Key(event) = event::read().unwrap() {
+                if event.code == event::KeyCode::Char('q') {
                     println!("Exiting...");
                     break;
                 }
@@ -71,6 +81,57 @@ fn main() {
     }
 
     disable_raw_mode().unwrap();
-    execute!(stdout, cursor::Show).unwrap();
-    execute!(stdout, terminal::LeaveAlternateScreen).unwrap();
+    execute!(
+        stdout,
+        cursor::Show,
+        terminal::LeaveAlternateScreen
+    ).unwrap();
+}
+
+fn draw_dots(stdout: &mut Stdout, width: u16, height: u16) {
+    for i in 0..=height {
+        for j in 0..=width {
+            queue!(
+                stdout,
+                MoveTo(j, i),
+                style::PrintStyledContent("·".grey())
+            ).unwrap();
+        }
+    }
+}
+
+fn draw_sine_wave(stdout: &mut Stdout, width: u16, height: u16, phase: f64) {
+    for i in 0..width {
+        let x = i as f64;
+        let y = (height as f64 / 2.0) + (phase + x / 10.0).sin() * 10.0;
+        let clamped_y = y as u16 % height;
+
+        queue!(
+            stdout,
+            MoveTo(x as u16, clamped_y),
+            style::PrintStyledContent("█".magenta())
+        ).unwrap();
+    }
+}
+
+fn draw_snow_flakes(stdout: &mut Stdout, width: u16, height: u16, phase: f64, dt: f64, snow_flakes: &mut Vec<Snowflake>) {
+    for snow_flake in snow_flakes.iter_mut() {
+        snow_flake.y += snow_flake.speed * dt;
+        snow_flake.x += (phase + snow_flake.x / 10.0).sin() * 0.3;
+
+        let x = snow_flake.x as u16;
+        let mut y = snow_flake.y as u16;
+
+        if y > height {
+            snow_flake.y = 0.0;
+            snow_flake.x = (width as f64 * rand::random::<f64>()).floor();
+            y = 0;
+        }
+
+        queue!(
+            stdout,
+            MoveTo(x, y),
+            style::PrintStyledContent("*".white())
+        ).unwrap();
+    }
 }
