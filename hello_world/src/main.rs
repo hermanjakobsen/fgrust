0763d8f086262f3f3782359be0d808a07b5f9082
fuglesang <me@fuglesang.dev>
2024-10-02 21:25:10 +0200

    cells

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -21,7 +21,20 @@ struct Snowflake {
     x: f64,
     y: f64,
     speed: f64,
-    sprite: String,
+    sprite: char,
+}
+
+struct Cell {
+    x: u16,
+    y: u16,
+    c: char,
+    color: style::Color,
+}
+
+impl Cell {
+    fn new(x: u16, y: u16, c: char, color: style::Color) -> Cell {
+        Cell { x, y, c, color }
+    }
 }
 
 const SANTA: &str = r#"
@@ -52,32 +65,31 @@ fn main() {
     let height = 30;
 
     let mut stdout = stdout();
-    let mut screen_buffer = vec![' '; (width * height) as usize];
-
-    // fill screen buffer with spaces
-    // for i in 0..screen_buffer.len() {
-    //     screen_buffer[i] = ' ' as usize;
-    // }
+    let mut screen_buffer: Vec<Cell> = Vec::new();
+    for i in 0..height {
+        for j in 0..width {
+            screen_buffer.push(Cell::new(j, i, ' ', style::Color::Rgb { r: 255, g: 255, b: 255 }));
+        }
+    }
 
     enable_raw_mode().unwrap();
 
-    queue!(
+    execute!(
         stdout,
         terminal::EnterAlternateScreen,
         cursor::Hide,
         EnableMouseCapture,
         Clear(ClearType::All)
     ).unwrap();
-    stdout.flush().unwrap();
 
-    let snow_flake_sprites = vec!["*", "·", "•"];
+    let snow_flake_sprites = vec!['*', '·', '•'];
     let mut snow_flakes: Vec<Snowflake> = Vec::new();
     for _ in 0..100 {
         snow_flakes.push(Snowflake {
             x: (width as f64 * rand::random::<f64>()).floor(),
-            y: (height as f64 * rand::random::<f64>()).floor(),
+            y: ((height-1) as f64 * rand::random::<f64>()).floor(),
             speed: (rand::random::<f64>() * 0.5) + 2.8,
-            sprite: snow_flake_sprites[(rand::random::<u16>() % snow_flake_sprites.len() as u16) as usize].to_string(),
+            sprite: snow_flake_sprites[(rand::random::<u16>() % snow_flake_sprites.len() as u16) as usize],
         });
     }
 
@@ -87,61 +99,55 @@ fn main() {
     let mut current_time = Instant::now();
     let mut mouse_position = (0, 0);
 
+
     loop {
         dt = delta_time(&mut current_time);
 
-        // queue!(
-        //     stdout,
-        //     MoveTo(0, 0),
-        //     Clear(ClearType::All)
-        // ).unwrap();
-
         phase += 1.0 * dt;
 
-        // draw_sine_wave(&mut stdout, width, height, phase);
-
-        draw_santa(&mut screen_buffer, width, height);
-        // draw_snow_flakes(&mut stdout, width, height, phase, dt, &mut snow_flakes);
-        draw_ground(&mut screen_buffer, width, height);
-        // draw_question(&mut stdout, width, height, mouse_position);
-
-        // draw screen buffer
-        for i in 0..height {
-            for j in 0..width {
-                let index = (i * width + j) as usize;
-                let c = screen_buffer[index];
-
-                queue!(
-                    stdout,
-                    MoveTo(j, i),
-                    style::PrintStyledContent(c.white())
-                ).unwrap();
-            }
+        // clear screen buffer
+        for i in 0..screen_buffer.len() {
+            screen_buffer[i].c = ' ';
         }
 
-        // queue!(
-        //     stdout,
-        //     MoveTo(0, 0),
-        //     style::PrintStyledContent(format!("Mouse: ({}, {})", mouse_position.0, mouse_position.1).stylize())
-        // ).unwrap();
-        // render mouse pos to screen buffer
+        // draw_sine_wave(&mut screen_buffer, width, height, phase);
+        draw_santa(&mut screen_buffer, width, height);
+        draw_snow_flakes(&mut screen_buffer, width, height, phase, dt, &mut snow_flakes);
+        draw_question(&mut screen_buffer, width, height, mouse_position);
+        draw_ground(&mut screen_buffer, width, height);
+
         let mouse_pos_str = format!("Mouse: ({}, {})", mouse_position.0, mouse_position.1);
         for (i, c) in mouse_pos_str.chars().enumerate() {
             let index = (0 * width + i as u16) as usize;
-            screen_buffer[index] = c;
+            screen_buffer[index].c = c;
         }
 
-        // queue!(
-        //     stdout,
-        //     MoveTo(0, 1),
-        //     style::PrintStyledContent(format!("FPS: {:.2}", 1.0 / dt).stylize())
-        // ).unwrap();
-        // render fps to screen buffer
         let fps_str = format!("FPS: {:.2}", 1.0 / dt);
         for (i, c) in fps_str.chars().enumerate() {
             let index = (1 * width + i as u16) as usize;
-            screen_buffer[index] = c;
+            screen_buffer[index].c = c;
         }
+
+        // render screen buffer cells
+        for cell in screen_buffer.iter() {
+            queue!(
+                stdout,
+                MoveTo(cell.x, cell.y),
+                SetForegroundColor(cell.color),
+                style::PrintStyledContent(cell.c.stylize())
+            ).unwrap();
+        }
+
+        // for i in 0..height {
+        //     let start = (i * width) as usize;
+        //     let end = ((i + 1) * width) as usize;
+        //     let line: String = screen_buffer[start..end].iter().map(|cell| cell.c).collect();
+        //     queue!(
+        //         stdout,
+        //         MoveTo(0, i),
+        //         style::PrintStyledContent(line.stylize())
+        //     ).unwrap();
+        // }
 
         stdout.flush().unwrap();
 
@@ -151,8 +157,7 @@ fn main() {
             let raw = read();
 
             if raw.is_err() {
-                println!("Error reading event: {:?}", raw.err());
-                break;
+                continue;
             }
 
             let event = raw.unwrap();
@@ -195,87 +200,70 @@ fn draw_dots(stdout: &mut Stdout, width: u16, height: u16) {
     }
 }
 
-fn draw_sine_wave(stdout: &mut Stdout, width: u16, height: u16, phase: f64) {
+fn draw_sine_wave(screen_buffer: &mut Vec<Cell>, width: u16, height: u16, phase: f64) {
     for i in 0..width {
         let x = i as f64;
         let y = (height as f64 / 2.0) + (phase + x / 10.0).sin() * 10.0;
         let clamped_y = y as u16 % height;
 
-        queue!(
-            stdout,
-            MoveTo(x as u16, clamped_y),
-            style::PrintStyledContent("█".magenta())
-        ).unwrap();
+        let index = (clamped_y * width + x as u16) as usize;
+        screen_buffer[index].c = '█';
     }
 }
 
-fn draw_ground(screen_buffer: &mut Vec<char>, width: u16, height: u16) {
+fn draw_ground(screen_buffer: &mut Vec<Cell>, width: u16, height: u16) {
     let ground = width * (height - 1);
 
     for i in 0..width {
-        screen_buffer[(ground + i) as usize] = '█';
+        screen_buffer[(ground + i) as usize].c = '█';
     }
 }
 
-fn draw_snow_flakes(stdout: &mut Stdout, width: u16, height: u16, phase: f64, dt: f64, snow_flakes: &mut Vec<Snowflake>) {
+fn draw_snow_flakes(screen_buffer: &mut Vec<Cell>, width: u16, height: u16, phase: f64, dt: f64, snow_flakes: &mut Vec<Snowflake>) {
     for snow_flake in snow_flakes.iter_mut() {
         snow_flake.y += snow_flake.speed * dt;
-        snow_flake.x += (phase + snow_flake.x / 10.0).sin() * 0.3;
+        snow_flake.x += (phase + snow_flake.x / 10.0).sin() * 0.02;
 
-        let mut x = snow_flake.x as u16;
+        let x = (snow_flake.x as u16).clamp(0, width-1);
         let y = snow_flake.y as u16;
 
-        if x > width-1 {
-            x = width-1;
-        }
-
-        if y >= height {
+        if y >= height-2 {
             snow_flake.y = 0.0;
-            snow_flake.x = (width as f64 * rand::random::<f64>()).floor();
+            snow_flake.x = (width - 1) as f64 * rand::random::<f64>();
         }
 
-        queue!(
-            stdout,
-            MoveTo(x, y),
-            style::PrintStyledContent(snow_flake.sprite.clone().white())
-        ).unwrap();
+        let index = ((y * width) + x) as usize;
+        screen_buffer[index].c = snow_flake.sprite;
     }
 }
 
-fn draw_santa(screen_buffer: &mut Vec<char>, width: u16, height: u16) {
+fn draw_santa(screen_buffer: &mut Vec<Cell>, width: u16, height: u16) {
     let lines = SANTA.lines();
     let x = 2;
     let offset = height - 1 - lines.clone().count() as u16;
 
     for (i, line) in lines.enumerate() {
-        if line == " " {
-            continue;
-        }
-
-
         for (j, c) in line.chars().enumerate() {
-            let index = ((offset + i as u16) * width + x + j as u16) as usize;
-            screen_buffer[index] = c;
-        }
+            if c == ' ' {
+                continue;
+            }
 
-        // queue!(
-        //     stdout,
-        //     MoveTo(5, offset + i as u16),
-        //     style::PrintStyledContent(line.white())
-        // ).unwrap();
+            let index = ((offset + i as u16) * width + x + j as u16) as usize;
+            screen_buffer[index].c = c;
+        }
     }
 }
 
-fn draw_question(stdout: &mut Stdout, width: u16, height: u16, mouse_position: (u16, u16)) {
+fn draw_question(screen_buffer: &mut Vec<Cell>, width: u16, height: u16, mouse_position: (u16, u16)) {
     let question = "What is the answer to life, the universe, and everything?";
-    draw_text_box(stdout, width, height, question, 0, -5, (0, 0));
+    draw_text_box(screen_buffer, width, height, question, 0, -5, (0, 0));
 
-    draw_text_box(stdout, width, height, "42", -20, 0, mouse_position);
-    draw_text_box(stdout, width, height, "24", 0, 0, mouse_position);
-    draw_text_box(stdout, width, height, "69", 20, 0, mouse_position);
+    draw_text_box(screen_buffer, width, height, "42", -20, 0, mouse_position);
+    draw_text_box(screen_buffer, width, height, "24", 0, 0, mouse_position);
+    draw_text_box(screen_buffer, width, height, "69", 20, 0, mouse_position);
 }
 
-fn draw_text_box(stdout: &mut Stdout, width: u16, height: u16, q: &str, x_offset: i16, y_offset: i16, mouse_position: (u16, u16)) {
+fn draw_text_box(screen_buffer: &mut Vec<Cell>, width: u16, height: u16, q: &str, x_offset: i16, y_offset: i16, mouse_position: (u16, u16)) {
     let question = q;
     let x_origin = ((width as i16 - question.len() as i16) / 2 + x_offset) as u16;
     let y_origin = (height as i16 / 2 + y_offset) as u16;
@@ -301,43 +289,48 @@ fn draw_text_box(stdout: &mut Stdout, width: u16, height: u16, q: &str, x_offset
         };
     }
 
-    queue!(
-        stdout,
-        SetForegroundColor(color),
-        MoveTo(x_origin - 3, y_origin - 1),
-        style::PrintStyledContent(fancy_top_border.stylize())
-    ).unwrap();
-
-    for i in 0..1 {
-        queue!(
-            stdout,
-            MoveTo(x_origin - 2 - 1, y_origin + i),
-            style::PrintStyledContent("│".stylize())
-        ).unwrap();
-
-        queue!(
-            stdout,
-            MoveTo(x_origin + question.len() as u16 + 2, y_origin + i),
-            style::PrintStyledContent("│".stylize())
-        ).unwrap();
+    for i in 0..3 {
+        let index = ((y_origin + i) * width + x_origin - 2) as usize;
+        let line = " ".repeat(question.len() + 4);
+        for (j, c) in line.chars().enumerate() {
+            screen_buffer[index + j].c = c;
+        }
     }
 
-    queue!(
-        stdout,
-        MoveTo(x_origin - 3, y_origin + 1),
-        style::PrintStyledContent(fancy_bottom_border.stylize())
-    ).unwrap();
+    {
+        let index = ((y_origin - 1) * width + x_origin - 3) as usize;
+        let line = &fancy_top_border;
+        for (j, c) in line.chars().enumerate() {
+            screen_buffer[index + j].c = c;
+            screen_buffer[index + j].color = color;
+        }
+    }
 
-    queue!(
-        stdout,
-        MoveTo(x_origin, y_origin),
-        style::PrintStyledContent(question.stylize())
-    ).unwrap();
+    {
+        let index = ((y_origin + 1) * width + x_origin - 3) as usize;
+        let line = &fancy_bottom_border;
+        for (j, c) in line.chars().enumerate() {
+            screen_buffer[index + j].c = c;
+            screen_buffer[index + j].color = color;
+        }
+    }
 
-    queue!(
-        stdout,
-        MoveTo(x_origin, y_origin),
-        style::PrintStyledContent(question.stylize()),
-        SetForegroundColor(style::Color::Reset)
-    ).unwrap();
+    {
+        let index = ((y_origin) * width + x_origin - 3) as usize;
+        screen_buffer[index].c = '│';
+        screen_buffer[index].color = color;
+    }
+
+    {
+        let index = ((y_origin) * width + x_origin + question.len() as u16 + 2) as usize;
+        screen_buffer[index].c = '│';
+        screen_buffer[index].color = color;
+    }
+
+
+    for (i, c) in question.chars().enumerate() {
+        let index = ((y_origin) * width + x_origin + i as u16) as usize;
+        screen_buffer[index].c = c;
+        screen_buffer[index].color = color;
+    }
 }
